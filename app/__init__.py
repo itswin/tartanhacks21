c5a5c7d66b371f583b35c3f036f44a556f08dc6e
Bharath Sreenivas <sreenivas.bharath@gmail.com>
2021-03-07 04:25:31 -0500

    added a very basic form to the design your own playlist section. got form submission to be recognized

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,12 @@
-from flask import Flask
 import os
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'C2HWGVoMGfNTBsrYQg8EcMrdTimkZfAb'
+Bootstrap(app)
 
 from app import run_app
