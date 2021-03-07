51da114bae008f887ddb11bedb037159a573b4cb
Winston Cheung <winston.cheung1@gmail.com>
2021-03-07 03:56:01 -0500

    Batched sentiment analysis working within Flask

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,4 +1,8 @@
 from google.cloud import language
+import threading
+import logging
+import requests
+from pprint import pprint
 
 def analyze_text_sentiment(text):
     client = language.LanguageServiceClient()
@@ -16,6 +20,48 @@ def analyze_text_sentiment(text):
     #     print(f"{k:10}: {v}")
     return results
 
+# Temporary solution to call the Natural Language API from flask
+def analyze_text_sentiment_workaround(text):
+    API_KEY = "AIzaSyCUDeQMDkbAkdKbKm9xNbANcxdr0iphfRI"
+    doc = {'type': 1, 'language': 'en', 'content': text}
+    d = {'document': doc, 'encodingType': 'UTF32'}
+    url = 'https://language.googleapis.com/v1beta2/documents:analyzeSentiment?key=' + API_KEY
+    response = requests.post(url, json=d, timeout=10.0).json()
+    
+    sentiment = response['documentSentiment']
+    results = dict(
+        text=text,
+        score=sentiment['score'],
+        magnitude=sentiment['magnitude']
+    )
+    
+    return results
+
+def analyze_text_sentiment_batch(texts):
+    n = len(texts)
+    lock = threading.Lock()
+    res = [{}] * n
+
+    def thread_function(text, idx):
+        results = analyze_text_sentiment_workaround(text)
+        with lock:
+            res[idx] = results
+    
+    threads = list()
+    for index in range(n):
+        logging.info("Main    : create and start thread %d.", index)
+        x = threading.Thread(target=thread_function, args=(texts[index], index,))
+        threads.append(x)
+        x.start()
+
+    for index, thread in enumerate(threads):
+        logging.info("Main    : before joining thread %d.", index)
+        thread.join()
+        logging.info("Main    : thread %d done", index)
+
+    return res
+
+
 def classify_text(text):
     client = language.LanguageServiceClient()
     document = language.Document(content=text, type_=language.Document.Type.PLAIN_TEXT)
@@ -29,4 +75,4 @@ def classify_text(text):
 
 if __name__ == '__main__':
     text = "Bharath is an amazing person"
-    analyze_text_sentiment(text)
+    # analysis = analyze_text_sentiment(text)
